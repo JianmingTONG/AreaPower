9d1d0fe17d25cc7b79854c87a1c2c4772b06741f
JianmingTONG <573925529@qq.com>
2020-11-19 13:42:45 +0800

    15nm

--- sequential_circuit/file_list.tcl
+++ sequential_circuit/file_list.tcl
@@ -8,5 +8,5 @@
 
 
 # For systemverilog files (pass parameters)
-analyze -format sverilog {../src/tree.v}
-elaborate -parameter "NUM= 4" tree
+analyze -format sverilog {../src/crossbar/c_select_1ofn.v ../src/crossbar/c_interleave.v ../src/crossbar/c_crossbar.v }
+elaborate -parameter "num_in_ports=4 ,num_out_ports=4 " c_crossbar
